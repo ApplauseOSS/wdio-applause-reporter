a45c497346f14b787cbb5ab45e6bbcb4affbd0c8
rconner46 <rconner@applause.com>
2024-07-24 14:33:13 -0400

    lock-common-reporter

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,7 @@
-/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@App/(.*)$': '<rootDir>/src/$1',
-  }
+  testEnvironment: "node",
+  transform: {
+    "test/^.+.tsx?$": ["ts-jest",{}],
+  },
 };
